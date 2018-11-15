2eefb067eda84fc36761013f71d8b6a033b53bb5
liuyanglsy <44664240+liuyanglsy@users.noreply.github.com>
2018-11-15 18:40:29 +0800

    Add files via upload

--- rikao.php
+++ rikao.php
@@ -6,5 +6,43 @@
  * Time: 8:35
  */
 
-$a=array(5,15,25);
-echo array_sum($a);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <table>
+            <tr>
+                <td><input type="button" value="谁最帅" onclick="dian()"></td>
+                <td><p id="d"></p></td>
+            </tr>
+            <tr>
+                <td><input type="button" value="谁最傻" onclick="ji()"></td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
+<script>
+    function dian(){
+        var q = "";
+        for(var i=0;i<10;i++)
+        {
+            q=q + "你猜" + i + "<br>";
+        }
+        document.getElementById("d").innerHTML=q;
+    }
+    function ji()
+    {
+        alert("甘少冬最傻");
+    }
+</script>
+
