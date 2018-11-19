ee4bcaec594fa8b3948748561c793e3517865cea
liuyanglsy <44664240+liuyanglsy@users.noreply.github.com>
2018-11-19 15:20:04 +0800

    Add files via upload

---
+++ add.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/19
+ * Time: 9:15
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<center>
+    <form action="show.php" method="post">
+        <table>
+            <tr>
+                <td>用户名</td>
+                <td><input type="text" name="user"></td>
+            </tr>
+            <tr>
+                <td>密码</td>
+                <td><input type="text" name="pwd"></td>
+            </tr>
+        </table>
+        <input type="submit" value="添加">
+    </form>
+</center>
+</body>
+</html>
--- show.php
+++ show.php
@@ -1,31 +1,47 @@
-<?php  
-function chou($num){
-  for($i = 2;;$i*=1){
-    if(is_int($num/2)){
-      $num = $num/2;
-      echo $num;
-    }else if($num == 1){
-      return '是丑数';
-    }else{
-      for($j = 3;;$j*=1){
-        if(is_int($num/$j)){
-          $num = $num/$j;
-        }else if($num == 1){
-          return '是丑数';
-        }else{
-          for($o = 5;;$o*=1){
-            if(is_int($num/$o)){
-              $num = $num/$o;
-            }else if($num == 1){
-              return '是丑数';
-            }else{
-              return '您输入的不是丑数';
-            }
-          }
-        }
-      }
-    }
-
-  }
-}
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/19
+ * Time: 9:19
+ */
+$pdo = new PDO("mysql:host=127.0.0.1;dbname=test","root","root");
+$sql = "SELECT * from zhoukao";
+$data = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<center>
+    <table border="1">
+        <input type="text">
+        <input type="button" value="搜索">
+        <tr>
+            <td>id</td>
+            <td>用户名</td>
+            <td>密码</td>
+            <td>操作</td>
+        </tr>
+        <?php foreach ($data as $v){ ?>
+            <tr>
+                <td><?php echo $v['id'] ?></td>
+                <td><?php echo $v['user'] ?></td>
+                <td><?php echo $v['pwd'] ?></td>
+                <td><a href="">删除</a></td>
+            </tr>
+        <?php } ?>
+    </table>
+    <a href="">首页</a>
+    <a href="">上一页</a>
+    <a href="">下一页</a>
+</center>
+</body>
+</html>
+
