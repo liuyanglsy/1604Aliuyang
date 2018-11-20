b61ac67fa6819b0f6bf5a38e7bea30a6cff79386
liuyanglsy <44664240+liuyanglsy@users.noreply.github.com>
2018-11-20 09:11:52 +0800

    Add files via upload

---
+++ rikao1.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/20
+ * Time: 8:23
+ */
+function NumberOf1($n)
+{
+    $count = 0;
+    $flag = 1;
+    while ($flag != 0) {
+        if (($n & $flag) != 0) {
+            $count++;
+        }
+        $flag = $flag << 1;
+    }
+    return $count;
+}
+$num=10;
+echo $num."的二进制是".decbin($num)."<br/>";
+echo $num."共有".NumberOf1($num)."个1";
